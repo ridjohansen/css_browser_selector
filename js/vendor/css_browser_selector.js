652c7c8df97c21700b87ceba4191bc7e4a749027
ridjohansen <ridjohansen@gmail.com>
2013-04-08 13:59:28 -0300

    Update css_browser_selector.js

--- js/vendor/css_browser_selector.js
+++ js/vendor/css_browser_selector.js
@@ -100,12 +100,12 @@ var uaInfo = {
 }
 
 var screenInfo = {
-	width : (window.outerWidth || html.clientWidth) - 8,
+	width : (window.outerWidth || html.clientWidth) - 16,
 	height : window.outerHeight || html.clientHeight,
 	screens : [0, 768, 980, 1200],
 	
 	screenSize : function () {
-		screenInfo.width = (window.outerWidth || html.clientWidth) - 8;
+		screenInfo.width = (window.outerWidth || html.clientWidth) - 16;
 		screenInfo.height = window.outerHeight || html.clientHeight;
 			
 		var screens = screenInfo.screens,
