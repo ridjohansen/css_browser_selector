1bea1b55779c8263c6855213c17c6062c13bcfc4
Igor Zenich <delka@ideus.biz>
2015-10-28 18:15:14 +0200

    Prevent global variables - wrap code into anonymous function, set css_browser_selector as a global object, to make tests work.

--- js/vendor/css_browser_selector.js
+++ js/vendor/css_browser_selector.js
@@ -9,6 +9,7 @@
  https://github.com/wbruno/css_browser_selector
 
  */
+(function() {
 var uaInfo = {
 	ua : '',
 	is : function (t) {
@@ -229,6 +230,10 @@ function css_browser_selector(u, ns) {
 
 // define css_browser_selector_ns before loading this script to assign a namespace
 var css_browser_selector_ns = css_browser_selector_ns || "";
+
+// Add css_browser_selector as a global object.
+window.css_browser_selector = css_browser_selector;
+})();
 
 // init
 css_browser_selector(navigator.userAgent, css_browser_selector_ns);
