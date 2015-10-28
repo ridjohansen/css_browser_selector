779d8af470c17415fd4992dd0442ccc52b590f20
Igor Zenich <delka@ideus.biz>
2015-10-28 18:07:20 +0200

    Add IE Edge Support Like `edge ie13 ie13_10565…`

--- js/vendor/css_browser_selector.js
+++ js/vendor/css_browser_selector.js
@@ -35,12 +35,14 @@ var uaInfo = {
 			a = 'android',
 			bb = 'blackberry',
 			dv = 'device_',
+			v = 0,
 
 			ua = uaInfo.ua,
 			is = uaInfo.is;
 
 		return [
 			(!(/opera|webtv/i.test(ua)) && /msie\s(\d+)/.test(ua)) ? ('ie ie' + (/trident\/4\.0/.test(ua) ? '8' : RegExp.$1))
+				:is('edge\/') ? 'edge ie' + (/edge\/(\d+)\.(\d+)/.test(ua) ? RegExp.$1 + ' ie' + RegExp.$1 + '_' + RegExp.$2 : '') // IE Edge
 				:is('trident\/') ? 'ie ie'+ (/trident\/.+rv:(\d+)/i.test(ua) ? RegExp.$1 : '') //ie11+
 				:is('firefox/') ? g + " " + f + (/firefox\/((\d+)(\.(\d+))(\.\d+)*)/.test(ua) ? ' ' + f + RegExp.$2 + ' ' + f + RegExp.$2 + "_" + RegExp.$4 : '')
 				:is('gecko/') ? g
